c8f96e167e612c64984e300778d79ec4b7797446
Samyak <121035286+Samyaktg@users.noreply.github.com>
2024-02-06 15:47:53 +0000

    more models!

--- app.py
+++ app.py
@@ -6,15 +6,26 @@ import numpy as np
 with open('model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
 
-with open('weather.pkl', 'rb') as model_file:
-    weat_her = pickle.load(model_file)
+with open('trip.pkl', 'rb') as model_file:
+    trip = pickle.load(model_file)
+
+with open('reason.pkl', 'rb') as model_file:
+    reason = pickle.load(model_file)
 
 # Your machine learning model function
 def predict(input_data):
     res = model.predict(input_data)
-    print("\n------Output-----\n")
     return res[0]
-   
+
+def tripping (trip_pred):     
+    x = trip.predict(trip_pred)
+    return x
+
+def reason_trip (reason_pred):    
+    y = reason.predict(reason_pred)
+    return y  
+    
+     
 
 # Streamlit app
 st.title("Your ML Model Web App")
@@ -27,31 +38,34 @@ Va = st.number_input("Line Voltage of Phase A:", step=0.1)
 Vb = st.number_input("Line Voltage of Phase B:", step=0.1)
 Vc = st.number_input("Line Voltage of Phase C:", step=0.1)
 sky=['clear', 'cloudy', 'foggy','rainy','semi cloudy','storm']
-weather = st.selectbox('How is the weather?', sky)
-
-
+weather = st.selectbox('Select an option:', sky)
+voltage = np.mean([Va,Vb,Vc],axis=0)
+trip_hour= st.slider('Select a time:', min_value=1, max_value=24, value=12)
 
 if weather == 'clear':
-    weather=0
+    weather = 0
 elif weather == 'cloudy':
-    weather=1
+    weather = 1
 elif weather == 'foggy':
-    weather=2
+    weather = 2
 elif weather == 'rainy':
-    weather=3
+    weather = 3
 elif weather == 'semi cloudy':
-    weather=4
+    weather = 4
 elif weather == 'storm':
-    weather=5
+    weather = 5
 
 # Input for user
 user_input = np.array([[float(Ia), float(Ib), float(Ic), float(Va), float(Vb), float(Vc)]])
 
-weather_data = np.array([[float(weather)]])
+is_trip = np.array([[float(weather),float(voltage),float(trip_hour)]])
 # Button to make predictions
 if st.button("Predict"):
     # Call the predict function with user input
     prediction = predict(user_input)
+    trip_pred = tripping(is_trip)
+
+
     st.write("Prediction:")
     if prediction[0] == 0:
         st.write("Line A Line B Line C")
@@ -68,6 +82,32 @@ if st.button("Predict"):
     else:
         st.write("Fault location uncertain")
 
+    st.write("Possible Issues:")
     
+    if trip_pred[0] == 0:
+        st.write("no trip")
+    else:
+        st.write("trip")
 
-
+    reason_input= np.array([[float(weather),float(voltage),float(trip_hour),float(trip_pred)]])
+    rreason_for_trip=reason_trip(reason_input)
+    if rreason_for_trip[0] == 0:
+        st.write("bad weather")
+    elif rreason_for_trip[0] == 1:
+        st.write("breaker opened")
+    elif rreason_for_trip[0] == 2:
+        st.write("earthing")
+    elif rreason_for_trip[0] == 3:
+        st.write("foreign element")
+    elif rreason_for_trip[0] == 4:
+        st.write("fuse failure") 
+    elif rreason_for_trip[0] == 5:
+        st.write("relay burn")  
+    elif rreason_for_trip[0] == 6:
+        st.write("transient fault")  
+    elif rreason_for_trip[0] == 7:
+        st.write("trip from kite")  
+    elif rreason_for_trip[0] == 8:
+        st.write("wire fallen")   
+    else:
+        st.write("Uncertain")
--- typeoffault.py
+++ typeoffault.py
@@ -1,4 +1,4 @@
-#Attempt to detect type of fault
+#og code of app.py
 
 import streamlit as st
 import pickle
@@ -16,14 +16,7 @@ def predict(input_data):
     res = model.predict(input_data)
     print("\n------Output-----\n")
     return res[0]
-
-def weather_preds (weather_data):    
-      
-    sam = weat_her.predict(weather_data)
-    return sam[0]
-    
-    
-     
+   
 
 # Streamlit app
 st.title("Your ML Model Web App")
@@ -36,9 +29,8 @@ Va = st.number_input("Line Voltage of Phase A:", step=0.1)
 Vb = st.number_input("Line Voltage of Phase B:", step=0.1)
 Vc = st.number_input("Line Voltage of Phase C:", step=0.1)
 sky=['clear', 'cloudy', 'foggy','rainy','semi cloudy','storm']
-weather = st.selectbox('Select an option:', sky)
-voltage = np.mean([Va,Vb,Vc],axis=0)
-trip_hour= st.slider('Select a time:', min_value=1, max_value=24, value=12)
+weather = st.selectbox('How is the weather?', sky)
+
 
 
 if weather == 'clear':
@@ -57,12 +49,11 @@ elif weather == 'storm':
 # Input for user
 user_input = np.array([[float(Ia), float(Ib), float(Ic), float(Va), float(Vb), float(Vc)]])
 
-weather_data = np.array([[float(weather),float(voltage),float(trip_hour)]])
+weather_data = np.array([[float(weather)]])
 # Button to make predictions
 if st.button("Predict"):
     # Call the predict function with user input
     prediction = predict(user_input)
-    weather_pred = weather_preds(weather_data)
     st.write("Prediction:")
     if prediction[0] == 0:
         st.write("Line A Line B Line C")
@@ -78,28 +69,5 @@ if st.button("Predict"):
         st.write("NO Fault")   
     else:
         st.write("Fault location uncertain")
-
-    st.write("Possible Issues:")
-    print("weather:",weather_pred)
-    if weather_pred[0] == 0:
-        st.write("bad weather")
-    elif weather_pred[0] == 1:
-        st.write("breaker opened")
-    elif weather_pred[0] == 2:
-        st.write("earthing")
-    elif weather_pred[0] == 3:
-        st.write("foreign element")
-    elif weather_pred[0] == 4:
-        st.write("fuse failure") 
-    elif weather_pred[0] == 5:
-        st.write("relay burn")  
-    elif weather_pred[0] == 6:
-        st.write("transient fault")  
-    elif weather_pred[0] == 7:
-        st.write("trip from kite")  
-    elif weather_pred[0] == 8:
-        st.write("wire fallen")   
-    else:
-        st.write("Uncertain")
 
 
